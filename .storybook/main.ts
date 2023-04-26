abc01ea106a44af1c7eb1237cf19e89565d629df
pouyio <pouyio@gmail.com>
2023-04-26 19:37:56 +0200

    updated gitignore

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  typescript: {
+    // if not unset, build failds
+    reactDocgen: false,
+  },
+};
+export default config;
